e55929a54297de1e0b09a569eb9ceec557f38d10
szewczukk <dmitro.szewczuk@gmail.com>
2024-03-09 18:08:33 +0100

    Show simple info on ProjectPage

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,12 +1,25 @@
 import { Link, useParams } from 'react-router-dom';
 import Button from '@/components/Button';
+import useProjectStore from '@/stores/project';
+import { useMemo } from 'react';
 
 export default function ProjectPage() {
+	const { projects } = useProjectStore();
 	const params = useParams<{ id: string }>();
+
+	const project = useMemo(
+		() => projects.find((p) => p.id === params.id),
+		[projects, params],
+	);
+
+	if (!project) {
+		return <h1>Not found</h1>;
+	}
 
 	return (
 		<div className="container mt-8 p-8 bg-emerald-200 mx-auto">
-			<h1>Project #{params.id}</h1>
+			<h1>Project {project.name}</h1>
+			<pre>{project.description}</pre>
 
 			<Link to="/">
 				<Button>Return to list</Button>
