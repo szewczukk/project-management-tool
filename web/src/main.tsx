f35f3be7915600272b797daa415769a2c39964c0
szewczukk <dmitro.szewczuk@gmail.com>
2024-06-12 22:50:42 +0200

    Remove unnecessary code

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import ProjectListPage from './pages/ProjectListPage';
-import ProjectPage from './pages/ProjectPage';
+import Dashboard from './pages/Dashboard';
 import './index.css';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		Component: ProjectListPage,
-	},
-	{
-		path: '/projects/:id',
-		Component: ProjectPage,
+		Component: Dashboard,
 	},
 ]);
 
